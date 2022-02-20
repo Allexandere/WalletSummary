eb86f7326a8377878fb209e7e4b9bbd4311f1bc2
Allexandere <39259989+Allexandere@users.noreply.github.com>
2022-02-20 14:40:08 +0300

    updated fegil url

--- src/main/java/io/comet/ws/adapters/WalletAdapter.java
+++ src/main/java/io/comet/ws/adapters/WalletAdapter.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient(value = "pythonadapter", url = "http://localhost:8000/")
+@FeignClient(value = "pythonadapter", url = "http://0.0.0.0:8000/")
 public interface WalletAdapter {
 
     @GetMapping(value = "/get_profile_info")
